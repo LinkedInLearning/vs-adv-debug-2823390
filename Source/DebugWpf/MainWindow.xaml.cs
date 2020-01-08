e08fe2b363b59ed72cb4a706580db13d5d875a42
walt-instructor <staffauthor@outlook.com>
2020-01-08 14:43:24 -0800

    Refactor the name generator project. redo the UI

--- Source/DebugWpf/MainWindow.xaml.cs
+++ Source/DebugWpf/MainWindow.xaml.cs
@@ -1,36 +1,28 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 
 namespace DebugWpf
 {
-	/// <summary>
-	/// Interaction logicc for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
-		private async void Button_Click(object sender, RoutedEventArgs e)
+
+		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			FastProgressBar.IsIndeterminate = true;
-			SlowProgressBar.IsIndeterminate = true;
 			var nameGen = new RobotNameGenerator.NameGenerator();
 
-			var roboNames = await nameGen.GetRobotNames(12);
-			FastListBox.ItemsSource = roboNames;
-			FastProgressBar.IsIndeterminate = false;
-			var slowNames = await nameGen.GetRobotNamesSlow(12);
-
-			
-			SlowListBox.ItemsSource = slowNames;
-			SlowProgressBar.IsIndeterminate = false;
+			var roboNames = nameGen.GetAllRobotNames();
+			TeamAListBox.ItemsSource = roboNames.Take(6);
+			TeamBListBox.ItemsSource = roboNames.Skip(6).Take(6);
 		}
 
 		private void ClearButton_Click(object sender, RoutedEventArgs e)
 		{
-			FastListBox.ItemsSource = null;
-			SlowListBox.ItemsSource = null;
+			TeamAListBox.ItemsSource = null;
+			TeamBListBox.ItemsSource = null;
 		}
 	}
 }
--- Source/RobotNameGenerator/NameGenerator.cs
+++ Source/RobotNameGenerator/NameGenerator.cs
@@ -1,81 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RobotNameGenerator
 {
-
 	public class NameGenerator
 	{
-
 		private List<string> _primeNames;
-		private List<string> _tagNames;
-
-
+		private List<string> _crewNames;
 		private Random _ran = new Random();
 
 		public NameGenerator()
 		{
-			_tagNames = System.IO.File.ReadAllLines("RobotTagNames.txt").ToList<string>();
+			_crewNames = System.IO.File.ReadAllLines("RobotCrewNames.txt").ToList<string>();
 			_primeNames = System.IO.File.ReadAllLines("RobotPrimeNames.txt").ToList<string>();
-
 		}
 
-		public string GetRobotName()
+		public RobotName GetRobotName()
 		{
 			var randomPrimeIndex = _ran.Next(1, _primeNames.Count);
-			var randomTagIndex = _ran.Next(0, _tagNames.Count);
-			return _primeNames[randomPrimeIndex] + " " + _tagNames[randomTagIndex];
+			var randomCrewIndex = _ran.Next(0, _crewNames.Count);
+			return new RobotName { PrimeName = _primeNames[randomPrimeIndex], CrewName = _crewNames[randomCrewIndex] };
 		}
-		public async Task<List<string>> GetRobotNames(int maxCount)
+
+		public List<RobotName> GetAllRobotNames()
 		{
+			var temp = new List<RobotName>();
 
-			var temp = new List<string>();
-			//int currentMaxCount = (Math.Min (_primeNames.Count * _tagNames.Count) > maxCount ? maxCount :
-			//                                                                (_primeNames.Count * _tagNames.Count);
-
-			int currentMaxCount = (Math.Min(Math.Min(_primeNames.Count, maxCount), _tagNames.Count));
 			var q1 = from name in _primeNames
 							 orderby _ran.NextDouble()
 							 select name;
-			var q2 = from name in _tagNames
+			var q2 = from name in _crewNames
 							 orderby _ran.NextDouble()
 							 select name;
 			var primeNameList = q1.ToList();
-			var tagNameList = q2.ToList();
-			for (int i = 0; i < currentMaxCount; i++)
+			var crewNameList = q2.ToList();
+			for (int i = 0; i < primeNameList.Count; i++)
 			{
-				temp.Add(primeNameList.ElementAt(i) + " " + tagNameList.ElementAt(i));
+				temp.Add(new RobotName { PrimeName = primeNameList.ElementAt(i), CrewName = crewNameList.ElementAt(i) });
 			}
-			await Task.Delay(_ran.Next(200, 300));
-			return temp;
-		}
 
-		public async Task<List<string>> GetRobotNamesSlow(int maxCount)
-		{
-
-			var temp = new List<string>();
-
-
-			int currentMaxCount = (Math.Min(Math.Min(_primeNames.Count, maxCount), _tagNames.Count));
-			var q1 = from name in _primeNames
-							 orderby _ran.NextDouble()
-							 select name;
-			var q2 = from name in _tagNames
-							 orderby _ran.NextDouble()
-							 select name;
-			var primeNameList = q1.ToList();
-			var tagNameList = q2.ToList();
-			for (int i = 0; i < currentMaxCount; i++)
-			{
-				temp.Add(primeNameList.ElementAt(i) + " " + tagNameList.ElementAt(i));
-			}
-			await Task.Delay(_ran.Next(1400, 2600));
 			return temp;
 		}
 
 
+		public struct RobotName
+		{
+			public string PrimeName { get; set; }
+			public string CrewName { get; set; }
+		}
 	}
 }

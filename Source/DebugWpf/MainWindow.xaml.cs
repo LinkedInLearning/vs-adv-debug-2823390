534e2dfd4070af98ffad0d2b5422da93a0f682ff
walt-instructor <staffauthor@outlook.com>
2020-01-12 23:14:49 -0800

    Rename solution folder, add sample code

--- Source/DebugWpf/MainWindow.xaml.cs
+++ Source/DebugWpf/MainWindow.xaml.cs
@@ -12,17 +12,41 @@ namespace DebugWpf
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			var nameGen = new RobotNameGenerator.NameGenerator();
+			decimal monthlyPayment;
+			decimal rate = 5.6M;
+			int numberMonths;
+			decimal loanAmount;
 
-			var roboNames = nameGen.GetAllRobotNames();
-			TeamAListBox.ItemsSource = roboNames.Take(6);
-			TeamBListBox.ItemsSource = roboNames.Skip(6).Take(6);
+			// step into
+			numberMonths = 12;
+			loanAmount = 1200;
+			monthlyPayment = Financial.CalculateMonthlyPayment(numberOfMonths: numberMonths,
+																												 loanRate: rate,
+																												 loanAmount: loanAmount);
+			ResultsListBox.Items.Add($"{loanAmount:C} loan for {numberMonths} months: {monthlyPayment:C}");
+
+			// step over
+			numberMonths = 4;
+			loanAmount = 2800;
+			monthlyPayment = Financial.CalculateMonthlyPayment(numberOfMonths: 24,
+																									 loanRate: rate,
+																									 loanAmount: 2800);
+			ResultsListBox.Items.Add($"{loanAmount:C} loan for {numberMonths} months: {monthlyPayment:C}");
+
+			// step out;
+			numberMonths = 36;
+			loanAmount = 3770;
+			monthlyPayment = Financial.CalculateMonthlyPayment(numberOfMonths: 36,
+																								 loanRate: rate,
+																								 loanAmount: 3700);
+			ResultsListBox.Items.Add($"{loanAmount:C} loan for {numberMonths} months: {monthlyPayment:C}");
+
 		}
 
-		private void ClearButton_Click(object sender, RoutedEventArgs e)
+			private void ClearButton_Click(object sender, RoutedEventArgs e)
 		{
-			TeamAListBox.ItemsSource = null;
-			TeamBListBox.ItemsSource = null;
+			ResultsListBox.ItemsSource = null;
+			
 		}
 	}
 }

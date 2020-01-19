07c9a61e76a3fd8fb32177bebd5bfb6a49a0c244
walt-instructor <staffauthor@outlook.com>
2020-01-18 16:25:32 -0800

    Add exception throw and catch code

--- Source/DebugWpf/Bank.cs
+++ Source/DebugWpf/Bank.cs
@@ -9,8 +9,17 @@ namespace DebugWpf.Financial
 	internal class OnlineServices
 	{
 		static Random _ran = new Random();
+		// simulated service
 		public static Decimal GetBankRateFromSystem()
 		{
+			if (_ran.NextDouble() < .2)
+			{
+				throw new System.Net.HttpListenerException(errorCode: 422, "Cannot start HTTP listener");
+			}
+			else if (_ran.NextDouble() < .4)
+			{
+				throw new System.Net.Http.HttpRequestException("There is trouble with the HTTP request.");
+			}
 			var currentRate = (decimal)_ran.NextDouble() * 4;
 			return currentRate;
 		}
--- Source/DebugWpf/MainWindow.xaml.cs
+++ Source/DebugWpf/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System.Windows;
-
+using System.Linq;
 namespace DebugWpf
 {
 	public partial class MainWindow : Window
@@ -17,38 +17,60 @@ namespace DebugWpf
 			decimal loanAmount;
 			string formattedResult;
 
-			// step into
+
 			numberMonths = 10;
 			loanAmount = 1100;
-			rate = LoanCalc.GetCurrentLoanRate();
-			monthlyPayment = LoanCalc.CalculateMonthlyPayment(numberOfMonths: numberMonths,
-																												 loanAmount: loanAmount,
-																												 loanRate: rate);
-			formattedResult = $"{loanAmount:C} loan (at {rate:P}) {numberMonths} months: {monthlyPayment:C}";
-			ResultsListBox.Items.Add(formattedResult);
+			try
+			{
+				rate = LoanCalc.GetCurrentLoanRate();
+				monthlyPayment = LoanCalc.CalculateMonthlyPayment(numberOfMonths: numberMonths,
+																													 loanAmount: loanAmount,
+																													 loanRate: rate);
+				formattedResult = $"{loanAmount:C} loan (at {rate:P}) {numberMonths} months: {monthlyPayment:C}";
+				ResultsListBox.Items.Add(formattedResult);
 
-			// step over
-			numberMonths = 4;
-			loanAmount = 2200;
-			monthlyPayment = LoanCalc.CalculateMonthlyPayment(numberOfMonths: numberMonths,
-																												 loanAmount: loanAmount,
-																												 loanRate: rate);
-			formattedResult = $"{loanAmount:C} loan (at {rate:P}) {numberMonths} months: {monthlyPayment:C}";
-			ResultsListBox.Items.Add(formattedResult);
 
-			// step out;
-			numberMonths = 36;
-			loanAmount = 3300;
-			monthlyPayment = LoanCalc.CalculateMonthlyPayment(numberOfMonths: numberMonths,
-				 																								loanAmount: loanAmount,
-																												loanRate: rate);
-			formattedResult = $"{loanAmount:C} loan (at {rate:P}) {numberMonths} months: {monthlyPayment:C}";
-			ResultsListBox.Items.Add(formattedResult);
+				numberMonths = 4;
+				loanAmount = 2200;
+				monthlyPayment = LoanCalc.CalculateMonthlyPayment(numberOfMonths: numberMonths,
+																													 loanAmount: loanAmount,
+																													 loanRate: rate);
+				formattedResult = $"{loanAmount:C} loan (at {rate:P}) {numberMonths} months: {monthlyPayment:C}";
+				ResultsListBox.Items.Add(formattedResult);
+
+
+				numberMonths = 36;
+				loanAmount = 3300;
+				monthlyPayment = LoanCalc.CalculateMonthlyPayment(numberOfMonths: numberMonths,
+																													 loanAmount: loanAmount,
+																													loanRate: rate);
+				formattedResult = $"{loanAmount:C} loan (at {rate:P}) {numberMonths} months: {monthlyPayment:C}";
+				ResultsListBox.Items.Add(formattedResult);
+			} catch (System.Net.Http.HttpRequestException ex)
+			{
+				MessageBox.Show($"{ex.GetType().Name}: {ex.Message}");
+			} catch (System.Net.HttpListenerException ex)
+			{
+				MessageBox.Show($"{ex.GetType().Name}: {ex.Message}");
+			} catch (System.Exception)
+			{
+				MessageBox.Show("Something unexpected happened in the application! Please restart the application.");
+				throw;
+			}
+			
 		}
 
 		private void ClearButton_Click(object sender, RoutedEventArgs e)
 		{
 			ResultsListBox.Items.Clear();
+		}
+
+		private void UnhandledButton_Click(object sender, RoutedEventArgs e)
+		{
+			var nameGen = new RobotNameGenerator.NameGenerator();
+			var nameList = nameGen.GetAllRobotNames();
+
+			var name = nameList.ElementAt(120);
 		}
 	}
 }

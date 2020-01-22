d9ba127597089c8bd1a45d196fe307c2c8d9bbce
walt-instructor <staffauthor@outlook.com>
2020-01-21 17:38:31 -0800

    Add CPU test code and parallel code to stress multiple cores.

---
+++ Source/DebugWpf/CpuTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugWpf
+{
+	class CpuTester
+	{
+		public static void UseTheCpu(int percent, int millisecondsToRun)
+		{
+			var stopTime = DateTime.Now.AddMilliseconds(millisecondsToRun);
+			if (percent < 0) { percent = 0; }
+			if (percent > 100) { percent = 100; }
+			var watch = new Stopwatch();
+			watch.Start();
+			while (DateTime.Now < stopTime)
+			{
+				// loop runs for percent milliseconds
+				// then sleeps for remaining percent
+
+				if (watch.ElapsedMilliseconds > percent)
+				{
+					Task.Delay(100 - percent);
+					watch.Reset();
+					watch.Start();
+				}
+			}
+		}
+	}
+}
--- Source/DebugWpf/MainWindow.xaml.cs
+++ Source/DebugWpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace DebugWpf
@@ -12,15 +14,24 @@ namespace DebugWpf
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
+			// stress one of the CPU cores
+			var percents = new List<int>() { 90 };
+			Parallel.ForEach(percents, (p) => CpuTester.UseTheCpu(p, 2000));
 			var nameGen = new RobotNameGenerator.NameGenerator();
 
+			// other code
 			var roboNames = nameGen.GetAllRobotNames();
 			TeamAListBox.ItemsSource = roboNames.Take(6);
 			TeamBListBox.ItemsSource = roboNames.Skip(6).Take(6);
 		}
 
-		private void ClearButton_Click(object sender, RoutedEventArgs e)
+		private void LoadFourButton_Click(object sender, RoutedEventArgs e)
 		{
+			// stress some of the CPU cores
+			var percents = new List<int>() { 30, 50, 80, 90 , 50};
+			Parallel.ForEach(percents, (p) => CpuTester.UseTheCpu(p, 2000));
+
+			// other code
 			TeamAListBox.ItemsSource = null;
 			TeamBListBox.ItemsSource = null;
 		}

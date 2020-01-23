0cfce5410ad12525942302dd7ff3d330a83b5dbf
walt-instructor <staffauthor@outlook.com>
2020-01-23 01:16:20 -0800

    Add Task and async demos

--- Source/DebugConsole/Program.cs
+++ Source/DebugConsole/Program.cs
@@ -6,18 +6,77 @@ namespace DebugConsole
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Start Work");
+			//	StartRobotsOneThread();
+			//StartRobotsParellel();
+			StartRobotsTask();
+			//await StartRobotAsync();
+			MainThreadWork();
+			Console.WriteLine("Robots Experiment Finished");
 
-			//var robot1 = new Robot(name: "Mingle", workEfficiency: .02, scanDelay: 500);
-			//var robot2 = new Robot(name: "Yodel", workEfficiency: .04, scanDelay: 35, laserColor: ConsoleColor.Yellow);
-			//robot1.PickupItems(10);
-			//robot2.PickupItems(30);
+		}
+
+		private static void MainThreadWork()
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			for (int counter = 0; counter < 3; counter++)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Task.Delay(20);
+				Console.WriteLine($"Main Thread. {counter}");
+			}
+			Console.ResetColor();
+		}
+		private static void StartRobotsParellel()
+		{
 			var robots = new List<Robot>();
-			robots.Add(new Robot(name: "Mingle", workEfficiency: .02, scanDelay: 500));
-			//Parallel.ForEach
-			Console.Read();
+			robots.Add(new Robot(name: "1-Mingle", itemCount: 10, workEfficiency: .02, scanDelay: 500));
+			robots.Add(new Robot(name: "2-Yodel", itemCount: 30, workEfficiency: .04, scanDelay: 35, laserColor: ConsoleColor.Yellow));
+			robots.Add(new Robot(name: "3-Squido", itemCount: 15, workEfficiency: .05, scanDelay: 135, laserColor: ConsoleColor.Cyan));
+			robots.Add(new Robot(name: "4-Spook", itemCount: 4, workEfficiency: .04, scanDelay: 500, laserColor: ConsoleColor.White));
+			Parallel.ForEach(robots, r => r.PickupItems()); ;
+
+
+		}
+
+		static void StartRobotsOneThread()
+		{
+			Console.WriteLine("Start Work on Main Thread");
+			var robot1 = new Robot(name: "1-Mingle", itemCount: 10, workEfficiency: .02, scanDelay: 500);
+			var robot2 = new Robot(name: "2-Yodel", itemCount: 12, workEfficiency: .04, scanDelay: 35, laserColor: ConsoleColor.Yellow);
+			var robot3 = new Robot(name: "3-Squido", itemCount: 15, workEfficiency: .05, scanDelay: 135, laserColor: ConsoleColor.Cyan);
+			var robot4 = new Robot(name: "4-Spook", itemCount: 4, workEfficiency: .04, scanDelay: 500, laserColor: ConsoleColor.White);
+			robot1.PickupItems();
+			robot2.PickupItems();
+			robot3.PickupItems();
+			robot4.PickupItems();
+		}
+		private static void StartRobotsTask()
+		{
+			var robot1 = new Robot(name: "1-Mingle", itemCount: 10, workEfficiency: .02, scanDelay: 500);
+			var robot2 = new Robot(name: "2-Yodel", itemCount: 12, workEfficiency: .04, scanDelay: 35, laserColor: ConsoleColor.Yellow);
+			var robot3 = new Robot(name: "3-Squido", itemCount: 7, workEfficiency: .05, scanDelay: 135, laserColor: ConsoleColor.Cyan);
+			var robot4 = new Robot(name: "4-Spook", itemCount: 4, workEfficiency: .04, scanDelay: 500, laserColor: ConsoleColor.White);
+			Task.Factory.StartNew(() => { robot1.PickupItems(); });
+			Task.Factory.StartNew(() => { robot2.PickupItems(); });
+		
+			var lastTask = Task.Factory.StartNew(() => { robot3.PickupItems(); });
+			lastTask.Wait();
+		}
+		
+		private static async Task StartRobotAsync()
+		{
+			var robot1 = new Robot(name: "1-Mingle", itemCount: 10, workEfficiency: .02, scanDelay: 500);
+			var robot2 = new Robot(name: "2-Yodel", itemCount: 12, workEfficiency: .04, scanDelay: 35, laserColor: ConsoleColor.Yellow);
+			var robot3 = new Robot(name: "3-Squido", itemCount: 7, workEfficiency: .05, scanDelay: 135, laserColor: ConsoleColor.Cyan);
+			var robot4 = new Robot(name: "4-Spook", itemCount: 4, workEfficiency: .04, scanDelay: 500, laserColor: ConsoleColor.White);
+			Task.Factory.StartNew(() => { robot1.PickupItems(); });
+			Task.Factory.StartNew(() => { robot2.PickupItems(); });
+			await Task.Factory.StartNew(() => { robot3.PickupItems(); });
+			await Task.Factory.StartNew(() => { robot4.PickupItems(); });
+			Task.Delay(2000);
 		}
 	}
+
 }
--- Source/DebugConsole/Robot.cs
+++ Source/DebugConsole/Robot.cs
@@ -18,12 +18,14 @@ namespace DebugConsole
 		// higher scan delay the longer
 		// it takes to locate item
 		public int ScanDelay { get; set; }
-		public void PickupItems(int itemCount)
+		public int ItemCount { get; set; }
+		public void PickupItems()
 		{
-			for (int counter = 1; counter < itemCount + 1; counter++)
+			for (int counter = 1; counter < ItemCount + 1; counter++)
 			{
 				if (BatteryLevel < 0.1)
 				{
+					Console.ForegroundColor = LaserColor;
 					Console.WriteLine($"{Name} battery low: Need to recharge");
 					break;
 				}
@@ -31,20 +33,22 @@ namespace DebugConsole
 				Console.WriteLine($"{Name} pickup #{counter:D3}");
 				Thread.Sleep(ScanDelay);
 				BatteryLevel = BatteryLevel - (DrainRate + WorkEfficiency);
-			
+
 			}
+			Console.ForegroundColor = LaserColor;
 			Console.WriteLine($"{Name} finished: Battery level: {BatteryLevel:N3} returning to base.");
 		}
 		public void ChargeBattery()
 		{
 			//Efficiency 
 		}
-		public Robot(string name, double workEfficiency, int scanDelay, ConsoleColor laserColor = ConsoleColor.Green)
+		public Robot(string name, int itemCount, double workEfficiency, int scanDelay, ConsoleColor laserColor = ConsoleColor.Green)
 		{
 			Name = name;
+			ItemCount = itemCount;
 			BatteryLevel = 1;
 			WorkEfficiency = workEfficiency;
-			ScanDelay = Math.Max (50,scanDelay);
+			ScanDelay = Math.Max(50, scanDelay);
 			LaserColor = laserColor;
 		}
 	}

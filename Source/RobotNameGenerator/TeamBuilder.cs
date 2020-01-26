b9ed03619b4c44c18b27baa891836470bab49f43
walt-instructor <staffauthor@outlook.com>
2020-01-26 00:18:27 -0800

    Add DND style dice roll for teams stats

--- Source/RobotNameGenerator/TeamBuilder.cs
+++ Source/RobotNameGenerator/TeamBuilder.cs
@@ -37,22 +37,35 @@ namespace RobotFactory
 		private List<Robot> _robotPool = new List<Robot>();
 		public List<Robot> GetRobots()
 		{
+			AssignRobotNames();
+			RollStats();
+
+			return _robotPool;
+		}
+
+		private void RollStats()
+		{
+			foreach (Robot robot in _robotPool)
+			{
+				robot.Endurance = RollDice(3);
+				robot.Speed = RollDice(3);
+				robot.Strength = RollDice(3);
+			}
+		}
+
+		private void AssignRobotNames()
+		{
 			var randomizedPrimeNames = GetRandomizedPrimeNames();
 			var randomizedCrewNames = GetRandomizedCrewNames();
 
 
 			for (int i = 0; i < randomizedPrimeNames.Count; i++)
 			{
-				//temp.Add(new Robot { PrimeName = GetRandomizedPrimeNames().ElementAt(i), CrewName = GetRandomizedCrewNames().ElementAt(i) });
-			
-				
+
 				_robotPool.ElementAt(i).PrimeName = randomizedPrimeNames.ElementAt(i);
 				_robotPool.ElementAt(i).CrewName = randomizedCrewNames.ElementAt(i);
 			}
-
-			return _robotPool;
 		}
-
 
 		private List<String> GetRandomizedPrimeNames()
 		{
@@ -69,10 +82,23 @@ namespace RobotFactory
 							 select name;
 			return q1.ToList();
 		}
+		private int RollDice(int diceCount)
+		{
+			int temp = 0;
+			for (int i = 0; i < diceCount; i++)
+			{
+				temp += _ran.Next(1, 6);
+			}
+			return temp;
+		}
 	}
+
 	public class Robot
 	{
 		public string PrimeName { get; set; }
 		public string CrewName { get; set; }
+		public int Speed { get; set; }
+		public int Strength { get; set; }
+		public int Endurance { get; set; }
 	}
 }

4f745b0917ba797f31678eb3a245948de5fed269
walt-instructor <staffauthor@outlook.com>
2020-01-26 00:02:14 -0800

    Refactor Generator into RobotFactory

--- Source/DebugWpf/MainWindow.xaml.cs
+++ Source/DebugWpf/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Windows;
 using System.Xml.Linq;
-using static RobotNameGenerator.NameGenerator;
+using RobotFactory;
 
 namespace DebugWpf
 {
@@ -15,14 +15,10 @@ namespace DebugWpf
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			var mySetting = Environment.ProcessorCount;
-			var all = Environment.GetEnvironmentVariables();
-
-			//MessageBox.Show(mySetting.ToString());
-
-			var nameGen = new RobotNameGenerator.NameGenerator();
-
-			var roboNames = nameGen.GetAllRobotNames();
+		
+			var nameGen = new RobotFactory.TeamBuilder();
+			
+			var roboNames = nameGen.GetRobots();
 			TeamAListBox.ItemsSource = roboNames.Take(6);
 			TeamBListBox.ItemsSource = roboNames.Skip(6).Take(6);
 		}
@@ -32,19 +28,15 @@ namespace DebugWpf
 			TeamAListBox.ItemsSource = null;
 			TeamBListBox.ItemsSource = null;
 		}
-
-		private void MessageButton_Click(object sender, RoutedEventArgs e)
+		private Random _ran = new Random();
+		private void ShowButton_Click(object sender, RoutedEventArgs e)
 		{
-			if (MessageBox.Show(messageBoxText: "Pick a member from each team?",
-													caption: "First team match",
-													icon: MessageBoxImage.Question,
-													button: MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-			{
+			
 				try
 				{
-					var teamAMember = (RobotName)(TeamAListBox.SelectedItem);
-					var teamBMember = (RobotName)(TeamBListBox.SelectedItem);
-					MatchTextBlock.Text = teamAMember.PrimeName + " vs. " + teamBMember.PrimeName;
+					var teamAMember = (Robot)(TeamAListBox.Items[_ran.Next(0,(TeamAListBox.Items.Count-1))]);
+					var teamBMember = (Robot)(TeamBListBox.Items[_ran.Next(0, (TeamBListBox.Items.Count - 1))]);
+				MatchTextBlock.Text = teamAMember.PrimeName + " vs. " + teamBMember.PrimeName;
 				} catch (NullReferenceException ex)
 				{
 					MessageBox.Show("Null value, did you select a team member from the listbox?");
@@ -55,7 +47,7 @@ namespace DebugWpf
 				}
 
 
-			}
+			
 		}
 	}
 }
---
+++ Source/RobotNameGenerator/TeamBuilder.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RobotFactory
+{
+	public class TeamBuilder
+	{
+		private List<string> _primeNames;
+		private List<string> _crewNames;
+		private Random _ran = new Random();
+
+		public TeamBuilder()
+		{
+
+			// read XML instead of text files
+			var xmlDoc = XDocument.Load("RobotNames.xml");
+
+
+			_crewNames = xmlDoc.Root.Elements("CrewNames").Elements("CrewName")
+																.Select(element => element.Value)
+																.ToList<string>();
+			_primeNames = xmlDoc.Root.Elements("PrimeNames").Elements("PrimeName")
+																.Select(element => element.Value)
+																.ToList<string>();
+			for (int nameCounter = 0; nameCounter < _primeNames.Count; nameCounter++)
+			{
+				_robotPool.Add(new Robot());
+			}
+		}
+		private void BuildRobotPool()
+		{
+
+		}
+		private List<Robot> _robotPool = new List<Robot>();
+		public List<Robot> GetRobots()
+		{
+			var randomizedPrimeNames = GetRandomizedPrimeNames();
+			var randomizedCrewNames = GetRandomizedCrewNames();
+
+
+			for (int i = 0; i < randomizedPrimeNames.Count; i++)
+			{
+				//temp.Add(new Robot { PrimeName = GetRandomizedPrimeNames().ElementAt(i), CrewName = GetRandomizedCrewNames().ElementAt(i) });
+			
+				
+				_robotPool.ElementAt(i).PrimeName = randomizedPrimeNames.ElementAt(i);
+				_robotPool.ElementAt(i).CrewName = randomizedCrewNames.ElementAt(i);
+			}
+
+			return _robotPool;
+		}
+
+
+		private List<String> GetRandomizedPrimeNames()
+		{
+			var q1 = from name in _primeNames
+							 orderby _ran.NextDouble()
+							 select name;
+			return q1.ToList();
+		}
+
+		private List<String> GetRandomizedCrewNames()
+		{
+			var q1 = from name in _crewNames
+							 orderby _ran.NextDouble()
+							 select name;
+			return q1.ToList();
+		}
+	}
+	public class Robot
+	{
+		public string PrimeName { get; set; }
+		public string CrewName { get; set; }
+	}
+}
